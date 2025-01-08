8436593e42eb6676a1081ad45d81be8cb83728a5
Tascu Daniel <tascudaniel22@stud.ase.ro>
2025-01-08 19:22:01 +0200

    First 4 tasks

---
+++ 1_1100_Tascu_Daniel.js
@@ -0,0 +1,131 @@
+const canvas = document.getElementById('desenareCanvas');
+const context = canvas.getContext('2d');
+const colorInput = document.getElementById('saveColor');
+const stergereBtn = document.getElementById('btnStergeCanvas');
+const saveBtn = document.getElementById('btnSalvareImage');
+const grosimeLinieIn = document.getElementById('selecteazaGrosime');
+const selectareForma = document.getElementById('selecteazaForma');
+
+
+
+let deseneaza = false;
+let initialX, initialY;
+let canvasSnapshot = null;
+
+// incepere desen
+canvas.addEventListener('mousedown', (e) => {
+    deseneaza = true;
+    initialX = e.offsetX;
+    initialY = e.offsetY;
+    canvasSnapshot = context.getImageData(0, 0, canvas.width, canvas.height);
+});
+
+// preview desen
+canvas.addEventListener('mousemove', (e) => {
+    if (deseneaza) {
+        context.putImageData(canvasSnapshot, 0, 0);
+       
+        const forma = selectareForma.value;
+        if (forma === 'elipsa') {
+            desenareElipsa(initialX, initialY, e.offsetX, e.offsetY, true);
+    } else if (forma === 'dreptunghi') {
+        desenareDreptunghi(initialX, initialY, e.offsetX, e.offsetY, true);
+    } else if (forma === 'linie') { 
+        desenareLinie(initialX, initialY, e.offsetX, e.offsetY, true);
+    }
+}
+});
+
+// finalizare desen
+canvas.addEventListener('mouseup', (e) => {
+    if (deseneaza) {
+        const forma = selectareForma.value; 
+        if (forma === 'elipsa') {
+            desenareElipsa(initialX, initialY, e.offsetX, e.offsetY, false);
+        } else if (forma === 'dreptunghi') {
+            desenareDreptunghi(initialX, initialY, e.offsetX, e.offsetY, false);
+        } else if (forma === 'linie') {
+            desenareLinie(initialX, initialY, e.offsetX, e.offsetY, false);
+        }
+        deseneaza = false;
+    }
+});
+
+// elipsa
+function desenareElipsa(x1, y1, x2, y2, Preview) {
+    const razaX = Math.abs(x2 - x1) / 2;
+    const razaY = Math.abs(y2 - y1) / 2;
+    const centruX = (x1 + x2) / 2;
+    const centruY = (y1 + y2) / 2;
+
+    context.beginPath();
+    context.ellipse(centruX, centruY, razaX, razaY, 0, 0, 2 * Math.PI);
+
+    if (Preview) {
+        context.strokeStyle = colorInput.value;
+        context.lineWidth = grosimeLinieIn.value;;
+        context.stroke();
+    } else {
+        context.fillStyle = colorInput.value;
+        context.fill();
+        // Salvează starea canvas-ului doar când desenul este finalizat
+        canvasSnapshot = context.getImageData(0, 0, canvas.width, canvas.height);
+    }
+}
+
+
+
+
+//functie desenare dreptunghi
+function desenareDreptunghi(x1,y1,x2,y2,preview)
+{
+    context.beginPath();
+    context.rect(x1,y1,x2-x1,y2-y1);
+    if(preview)
+    {
+        context.strokeStyle = colorInput.value;
+        context.lineWidth = grosimeLinieIn.value;
+        context.stroke();
+    }
+    else
+    {
+        context.fillStyle = colorInput.value;
+        context.fill();
+        canvasSnapshot = context.getImageData(0, 0, canvas.width, canvas.height);
+    }
+}
+
+//functie desenare linie
+function desenareLinie(x1,y1,x2,y2,preview)
+{
+    context.beginPath();
+    context.moveTo(x1,y1);
+    context.lineTo(x2,y2);
+    if(preview)
+    {
+        context.strokeStyle = colorInput.value;
+        context.lineWidth = grosimeLinieIn.value;
+        context.stroke();
+    }
+    else
+    {
+        context.strokeStyle = colorInput.value;
+        context.lineWidth = grosimeLinieIn.value;
+        context.stroke();
+        canvasSnapshot = context.getImageData(0, 0, canvas.width, canvas.height);
+    }
+}
+
+
+stergereBtn.addEventListener('click', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+// png save
+saveBtn.addEventListener('click', () => {
+    const imagineGenerata = canvas.toDataURL('image/png');
+    const linkSalvare = document.createElement('a');
+    linkSalvare.download = 'imagine_desen.png';
+    linkSalvare.href = imagineGenerata;
+    linkSalvare.click();
+});
